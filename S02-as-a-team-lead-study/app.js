d47e648d276f11ca35cd10522fabc916ce27a7fe
Adam <adem.bekci.28@gmail.com>
2024-03-07 19:07:58 +0300

    intro butonu da tamam

--- S02-as-a-team-lead-study/app.js
+++ S02-as-a-team-lead-study/app.js
@@ -27,7 +27,7 @@ let arr = [
   "Q5187-MUSTAFA",
   "QA175-CEREN",
   "QC101-ZAFER",
-  "QA158-ADEM"
+  "QA158-ADEM",
 ];
 const imgObj = {
   QA124: "https://ca.slack-edge.com/T06BXPTNG1H-U06BZMJ6NRG-dccbd667afa4-512",
@@ -47,7 +47,7 @@ const imgObj = {
   Q5187: "https://ca.slack-edge.com/T06BXPTNG1H-U06BPTAS2R5-e3d695199afa-512",
   QA175: "./img/QA175.jpg",
   QC101: "./img/QC101.jpg",
-  QA158:"https://ca.slack-edge.com/T06BXPTNG1H-U06C0S2BFGB-32c50024b778-192"
+  QA158: "https://ca.slack-edge.com/T06BXPTNG1H-U06C0S2BFGB-32c50024b778-192",
 };
 let interval;
 btn.addEventListener("click", () => {
@@ -93,5 +93,7 @@ videoBtn.addEventListener("click", () => {
   document.querySelector("#introVideo").play();
   setTimeout(() => {
     modalVideo.style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("question1").style.display = "block";
   }, 16500);
 });
